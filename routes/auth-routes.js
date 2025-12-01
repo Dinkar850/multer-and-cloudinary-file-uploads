61ff6ebb528ecd8fb4f467e08c02e72e0037787a
Dinkar850 <guptadinkar850@gmail.com>
2025-12-01 05:33:54 +0530

    added more features: pagination, image deletion, changing password

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/auth-controller");
+const {
+  registerUser,
+  loginUser,
+  changePassword,
+} = require("../controllers/auth-controller");
+const authMiddleware = require("../middlewares/auth-middleware");
 const router = express.Router();
 
 //all routes related to authentication and authorization
 router.post("/register", registerUser); // /api/auth/register
 router.post("/login", loginUser); // /api/auth/login
-
+router.post("/change-password", authMiddleware, changePassword);
 module.exports = router;
